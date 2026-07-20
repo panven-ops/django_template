674b118da41fe1897b8ad2a78719a8795aa1e875
panven-ops <venizeleaspanos@gmail.com>
2026-07-20 08:56:51 +0300

    fix: fixed stateful env vars and pgdata path

--- manage.py
+++ manage.py
@@ -1,5 +1,3 @@
-#!/usr/bin/env python
-"""Django's command-line utility for administrative tasks."""
 import os
 import sys
 from dotenv import load_dotenv
@@ -7,7 +5,6 @@ from dotenv import load_dotenv
 
 def main():
     load_dotenv()
-    """Run administrative tasks."""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
     try:
         from django.core.management import execute_from_command_line
